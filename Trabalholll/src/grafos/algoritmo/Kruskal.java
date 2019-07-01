8a205ca1e6d6e91308b4dd4b4bd6dc41ae6180af
leticiawoelfer <leticia.woelfer@hotmail.com>
2019-07-01 01:19:21 -0300

    Trabalho3 - Questão 01 Finalizada

--- Trabalholll/src/grafos/algoritmo/Kruskal.java
+++ Trabalholll/src/grafos/algoritmo/Kruskal.java
@@ -1,18 +1,87 @@
 package grafos.algoritmo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 
  * @author gabriela.sena, leticia.woelfer
  *
  */
-public class Kruskal {
+/*public class Kruskal {
 
+	public static void main(String[] args) throws IOException {
+		new Kruskal().processar();
+	}
+
+	public final String caminhoArquivo = "c:\\temp\\entrada.in";
+
+	public void processar() throws IOException {
+		List<String> linhas = Files.readAllLines(Paths.get(caminhoArquivo));
+		boolean primeiraLinha = true;
+		List<Aresta> arestas = new ArrayList<>();
+		String conteudoArquivo = "";
+		for (String conteudoLinha : linhas) {
+			String linha[] = conteudoLinha.trim().split(" ");
+			if (primeiraLinha) {
+				if (linha.length >= 3) {
+					System.err.println("Arquivo inválido. A primeira linha deve conter 3 caracters");
+					return;
+				}
+				primeiraLinha = false;
+				conteudoArquivo += conteudoLinha + "\n";
+				continue;
+			}
+
+			if (linha.length >= 4) {
+				System.err.println("Conteudo da linha inválido, deve conter 3 "
+						+ "posições contendo 'Vertice Vertice Valor'");
+				return;
+			}
+
+			int verticeOrigem = Integer.parseInt(linha[0]);
+			int verticeDestino = Integer.parseInt(linha[1]);
+
+			int valor = Integer.parseInt(linha[2]);
+			Aresta edge = new Aresta(verticeOrigem, verticeDestino, valor);
+			arestas.add(edge);
+			conteudoArquivo += conteudoLinha + "\n";
+		}
+		Grafo grafo = new Grafo(arestas.toArray(new Aresta[arestas.size()]));
+		int somaArvoreGeradoraMinima = kruskal(grafo, arestas);
+
+		System.out.println("Entrada\n" + conteudoArquivo);
+		System.out.println("Saída\n" + somaArvoreGeradoraMinima);
+	}
+
+	private int kruskal(Grafo grafo, List<Aresta> listaArestas) {
+		List<Vertice> listaVertices = Arrays.asList(grafo.getVertices());
+		ConjuntoDisjunto conjuntoDisjunto = new ConjuntoDisjunto(listaVertices);
+	    int somaMenorCaminho = 0;
+	    
+	    //Ordena crescentemente as arestas de acordo com o valor
+	    Collections.sort(listaArestas);
+	    System.out.println("listaArestas: "+listaArestas.toString());
+		for (Aresta aresta : listaArestas) {
+			Vertice origem = listaVertices.get(aresta.getOrigem());
+			Vertice destino = listaVertices.get(aresta.getDestino());
+			//se origem e destino nao sao do mesmo conjunto, então unir e somar o valor
+			if (conjuntoDisjunto.procurar(origem) != conjuntoDisjunto.procurar(destino)) {
+				conjuntoDisjunto.unir(origem, destino);
+				somaMenorCaminho = somaMenorCaminho + aresta.getPeso();
+			}
+		}
+		return somaMenorCaminho;
+	}
 }
 
-class Aresta {
-	
+class Aresta implements Comparable<Aresta> {
+
 	private int origem;
 	private int destino;
 	private int peso;
@@ -42,6 +111,21 @@ class Aresta {
 			return this.destino;
 		}
 	}
+
+	@Override
+	public int compareTo(Aresta aresta) {
+		// 0:date1 == date2 , 1:date1 > date2, -1:date1 < date2
+		if (this.getPeso() < aresta.getPeso())
+			return -1;
+		else if (this.getPeso() == aresta.getPeso())
+			return 0;
+		return 1;
+	}
+	
+	@Override
+	public String toString() {		
+		return getOrigem()+","+getDestino()+"("+getPeso()+")";
+	}
 }
 
 class Grafo {
@@ -63,49 +147,10 @@ class Grafo {
 		}
 	}
 
-	public void calcularDistancia(int origem) {
-		this.vertices[origem].setDistancia(0);
-		int proximoVertice = origem;
-
-		for (int i = 0; i < this.vertices.length; i++) {
-			ArrayList<Aresta> arestasAtuais = this.vertices[proximoVertice].getArestas();
-			for (int vertice = 0; vertice < arestasAtuais.size(); vertice++) {
-				int visinho = arestasAtuais.get(vertice).getVizinho(proximoVertice);
-	
-				if (!this.vertices[visinho].isVisitado()) { /**/
-					int tentative = this.vertices[proximoVertice].getDistancia() + arestasAtuais.get(vertice).getPeso();
-					if (tentative < vertices[visinho].getDistancia()) {
-						vertices[visinho].setDistancia(tentative);
-					}
-				}
-			}
-			
-			vertices[proximoVertice].setVisitado(true);
-			proximoVertice = getMenorCaminho();
-		}
-	}
-
-	private int getMenorCaminho() {
-		int indice = 0;
-		int distancia = Integer.MAX_VALUE;
-		for (int i = 0; i < this.vertices.length; i++) {
-			int distanciaAtual = this.vertices[i].getDistancia();
-			if (!this.vertices[i].isVisitado() && distanciaAtual < distancia) {
-				distancia = distanciaAtual;
-				indice = i;
-			}
-		}
-		return indice;
-	}
-	
-	public int getCaminhoMinimo(int destino) {
-		return this.vertices[destino].getDistancia();
-	}
-	
 	public Vertice getVerticeComCaminhoMinimo(int destino) {
 		return this.vertices[destino];
 	}
-	
+
 	private int calculaQtdVertice(Aresta[] arestas) {
 		int vertice = 0;
 		for (Aresta aresta : arestas) {
@@ -118,16 +163,6 @@ class Grafo {
 		return vertice;
 	}
 
-
-	public String getResultado() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(String.format("Quantidade de vertices: %s. Quantidade de arestas: %s", this.qtdVertice, this.qtdAresta)).append("\n");
-		for (int i = 0; i < this.vertices.length; i++) {
-			sb.append(String.format("Distancia entre origem 0 e destino '%s' : '%s'", i, vertices[i].getDistancia())).append("\n");
-		}
-		return sb.toString();
-	}
-
 	public Vertice[] getVertices() {
 		return vertices;
 	}
@@ -135,32 +170,22 @@ class Grafo {
 	public void setVertices(Vertice[] vertices) {
 		this.vertices = vertices;
 	}
-	
-	
+
 }
 
-class Vertice implements Comparable<Vertice>{
-	
-	private int distancia = Integer.MAX_VALUE;
+class Vertice {
+
 	private boolean visitado = false;
 	private int codigo;
 	private ArrayList<Aresta> arestas = new ArrayList<Aresta>();
 	private Vertice pai;
-	
+
 	public Vertice(int codigo) {
 		this.codigo = codigo;
-	}	
+	}
 
 	public int getCodigo() {
 		return codigo;
-	}
-
-	public int getDistancia() {
-		return distancia;
-	}
-
-	public void setDistancia(int distancia) {
-		this.distancia = distancia;
 	}
 
 	public boolean isVisitado() {
@@ -187,15 +212,74 @@ class Vertice implements Comparable<Vertice>{
 		this.pai = pai;
 	}
 
-	@Override
-	public int compareTo(Vertice vertice) {
-		if (this.getDistancia() < vertice.getDistancia())
-			return -1;
-		else if (this.getDistancia() == vertice.getDistancia())
-			return 0;
-		return 1;
+	public int getGrau() {
+		return arestas.size();
 	}
-	
+
 }
 
+class No {
+	int grau;
+	int indice;
+	No pai;
 
+	public No(int grau, int id, No pai) {
+		this.grau = grau;
+		this.indice = id;
+		this.pai = pai;
+	}
+}
+
+class ConjuntoDisjunto {
+
+	List<Vertice> listaVerts;
+
+	public ConjuntoDisjunto(List<Vertice> vertices) {
+		this.listaVerts = vertices;
+	}
+
+	public int procurar(Vertice vertice) {
+		while (vertice.getPai() != null)
+			vertice = vertice.getPai();
+
+		return vertice.getCodigo();
+	}
+
+	public void unir(Vertice origem, Vertice destino) {
+		int codigoOrigem = procurar(origem);
+		int codigoDestino = procurar(destino);
+
+		// Vertices são iguais, logo do mesmo conjunto
+		if (codigoOrigem == codigoDestino)
+			return;
+
+		// Busca os vértices pais na lista de vértices
+		Vertice vertA = getVertice(codigoOrigem);
+		Vertice vertB = getVertice(codigoDestino);
+
+		// Concatena o menor grafo no maior grafo
+		try {
+			if (vertA.getGrau() < vertB.getGrau()) {
+				vertA.setPai(vertB);
+			} else if (vertA.getGrau() > vertB.getGrau()) {
+				vertB.setPai(vertA);
+			} else {
+				vertB.setPai(vertA);
+				// vertA.getGrau()++; //nao entendi isso aqui
+			}
+		} catch (NullPointerException e) {
+			System.err.println("Ocorreu um erro! O algoritmo não encontrou o vértice desejado na lista de vértices.");
+		}
+		
+	}
+
+	private Vertice getVertice(int codigo) {
+		for (Vertice vertice : listaVerts) {
+			if (vertice.getCodigo() == codigo) {
+				return vertice;
+			}
+		}
+		return null;
+	}
+}
+*/
